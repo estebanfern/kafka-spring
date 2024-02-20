bf3441984ee8268f8aa8a3bf78614d0bf90142b7
Esteban Fernandez <efernandez@roshka.com>
2024-02-20 16:32:50 -0300

    fix global advicer

--- src/main/java/org/efernandez/kafkaspring/advicer/GlobalAdvicer.java
+++ src/main/java/org/efernandez/kafkaspring/advicer/GlobalAdvicer.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalAdvicer {
 
     @ExceptionHandler(Throwable.class)
-    public BaseResponse handleException(Exception e) {
+    public BaseResponse handleException(Throwable e) {
         return new BaseResponse(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
